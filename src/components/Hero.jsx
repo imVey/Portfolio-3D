14fc81586dd0dca7a396f0d1be522413aa0c3bbb
imVey <marwanbella91@hotmail.fr>
2023-10-03 09:44:59 +0200

    Section and motion added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <div>
         <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Marwan</span></h1>
         <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I develop 3D visuals, user <br className="sm:block hidden" />interfaces and web applications
+          I develop 3D visuals, user <br className="sm:block hidden" />interfaces and web applications.
         </p>
       </div>
      
